def79af08816cf7a914376b2f0c8a2d6bf5252c7
krutikadussawar <krutikadussawar1204@gmail.com>
2025-07-19 15:06:45 +0530

    new update

--- src/Practice1/demo2.java
+++ src/Practice1/demo2.java
@@ -4,8 +4,15 @@ public class demo2
 {
 	public static void main(String[] args) 
 	{
+		int a=10;
+		int b =12;
 		System.out.println("hello");
 		
+	}
+	
+	public static void add(int a,int b)
+	{
+		System.out.println(a+b);
 	}
 
 }
