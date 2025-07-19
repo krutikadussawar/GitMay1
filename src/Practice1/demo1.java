e12eb65456fdc466004d2b607cd8813a538dae5c
krutikadussawar <krutikadussawar1204@gmail.com>
2025-07-19 14:43:59 +0530

    hello

---
+++ src/Practice1/demo1.java
@@ -0,0 +1,18 @@
+package Practice1;
+
+public class demo1 
+{
+	public static void main(String[] args)
+	{
+		int a=12;
+		int b=10;
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
